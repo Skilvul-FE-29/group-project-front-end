070a14af189a245a1f0c6abc3c52b68a156eceb3
Fitron Ansori <fitronansori7@gmail.com>
2022-11-14 20:49:07 +0800

    call teachers api and make card

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Navbar.css";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return <div className="navbar"></div>;
 }
 
 export default Navbar;
--- src/pages/TeacherList.jsx
+++ src/pages/TeacherList.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./TeacherList.css";
+import TeacherCard from "../components/list-teacher/TeacherCard";
+import Navbar from "../components/navbar/Navbar";
+// import axios from "axios";
 
 function TeacherList() {
-  return <div>TeacherList</div>;
+  return (
+    <>
+      <Navbar />
+      <div className="teacher-container">
+        <h1 className="teacher-title">Daftar Pengajar</h1>
+        <div className="search-teacher-wrapper">
+          <label htmlFor="search-input" className="search-title">
+            Search Pengajar
+          </label>
+          <input
+            type="text"
+            id="search-input"
+            placeholder="nama/jurusan/lokasi"
+            autoComplete="off"
+          />
+        </div>
+
+        <TeacherCard />
+      </div>
+    </>
+  );
 }
 
 export default TeacherList;
