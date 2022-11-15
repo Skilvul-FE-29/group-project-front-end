9ca906558233412419e89e30869941e754fb360c
pramanarendra <albertushandypramana@gmail.com>
2022-11-15 13:14:33 +0700

    membuat navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 // import Navbar from "./components/navbar/Navbar";
 import TeacherList from "./pages/TeacherList";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <TeacherList />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navbar />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import '../styles/navbar.css'
+
+function Navbar() {
+
+  const [isToggle, setToggle] = useState(false);
+  const handleToggle = () => {
+    setToggle(!isToggle);
+  }
+
+  return (
+    <nav>
+      <Link to='/'>
+        <p className="nav-logo">Ajarin</p>
+      </Link>
+      <ul className={isToggle ? "nav-menu" : "nav-menu active"}>
+        <li><Link to="/">Pesan Les</Link></li>
+        <li><Link to="/">Daftar Pesanan</Link></li>
+        <li><Link to="/">Login</Link></li>
+        <li><Link to="/">Register</Link></li>
+      </ul>
+      <div onClick={handleToggle} className={isToggle ? "hamburger" : "hamburger active"}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar;
