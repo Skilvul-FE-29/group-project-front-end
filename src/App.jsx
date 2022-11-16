e4766cf385d29f34cb2efd0cac1d79725d075790
Fitron Ansori <fitronansori7@gmail.com>
2022-11-16 20:54:32 +0800

    update struktur folder fitron

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-// import Navbar from "./components/navbar/Navbar";
-import TeacherList from "./pages/TeacherList";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Homepage from "./pages/Homepage";
 import Loginpage from "./pages/Loginpage";
 import Regispage from "./pages/Regispage";
+import TeacherList from "./pages/TeacherList";
+import RequestList from "./pages/RequestList";
 
 function App() {
   return (
@@ -15,6 +15,8 @@ function App() {
           <Route path="/" element={<Homepage />}></Route>
           <Route path="/login" element={<Loginpage />}></Route>
           <Route path="/regis" element={<Regispage />}></Route>
+          <Route path="/teacher" element={<TeacherList />}></Route>
+          <Route path="/request" element={<RequestList />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
