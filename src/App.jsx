1607ed32cd236e77b588f7002d4177ddaac859be
putridresty <putridresty25@gmail.com>
2022-11-18 14:44:27 +0700

    add payment-page

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import TeacherList from "./pages/TeacherList";
 import RequestList from "./pages/RequestList";
 import Schedule from "./pages/Schedule";
 import OrderForm from "./pages/OrderForm";
+import PaymentPage from "./pages/PaymentPage";
 
 function App() {
   return (
@@ -23,6 +24,7 @@ function App() {
           <Route path="/request" element={<RequestList />}></Route>
           <Route path="/schedule" element={<Schedule />}></Route>
           <Route path="/order-form" element={<OrderForm />}></Route>
+          <Route path="/payment" element={<PaymentPage />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/pages/PaymentPage.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import dana from "../images/dana.png";
+import linkaja from "../images/linkaja.png"
+import '../styles/PaymentPage.css'
+
+const PaymentPage = () => {
+  return (
+    <div className='payment'>
+        <section className="left">
+            <h2>Pesanan Kamu</h2>
+
+            <div className="name">
+                <h3>Dipesan oleh : </h3>
+                <p>Putri Dresty</p>
+            </div>
+            <div className="class-name">
+                <h3>Nama Kelas : </h3>
+                <p>Matematika Aljabar</p>
+            </div>
+            <div className="mentor">
+                <h3>Nama Mentor : </h3>
+                <p>Putri Dresty</p>
+            </div>
+            <div className="sum-peserta">
+                <h3>Jumlah Peserta : </h3>
+                <p>1 orang/sesi</p>
+            </div>
+            <div className="loc">
+                <h3>Lokasi Pembelajaran : </h3>
+                <p>Online - Zoom Meeting</p>
+            </div>
+        </section>
+        <section className="right">
+            <div className="title">
+                <h1 className='title-h1'>Pembayaran</h1>
+                <p className='title-p'>Silahkan pilih pembayaran yang tertera dibawaah :</p>
+            </div>
+            <div className="card">
+                <div className="card-payment">
+                    <img src={dana} alt=""/>
+                    <div className="info-payment">
+                        <p>DANA</p>
+                        <p>089990990876</p>
+                        <p>A/N. Putri Dresty</p>
+                    </div>
+                </div>
+                <div className="card-payment">
+                        <img src={linkaja} alt=""/>
+                        <div className="info-payment">
+                            <p>LINK AJA</p>
+                            <p>089990990876</p>
+                            <p>A/N. Putri Dresty</p>
+                        </div>
+                    </div>
+                <form action="" className='form-bukti'>
+                    <div className="card-bukti">
+                        <h3>Total Pembayaran : Rp 100.000-,</h3>
+                        <label htmlFor="up-bukti">Upload Bukti Pembayaran :</label>
+                        <input type="file" name='up-bukti'/>
+                    </div>
+                    <button className='btn-bayar'>Bayar Sekarang</button>
+                </form>
+            </div>
+        </section>
+    </div>
+  )
+}
+
+export default PaymentPage
