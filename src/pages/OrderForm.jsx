a6cbe7b73707b58cb6504468409273ff8969af2d
pramanarendra <albertushandypramana@gmail.com>
2022-11-19 23:21:26 +0700

    revisi data input dan styling

--- src/pages/OrderForm.jsx
+++ src/pages/OrderForm.jsx
@@ -1,78 +1,102 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import '../styles/orderform.css'
 
 const OrderForm = () => {
-  return (
-    <div className="container">
-      <div className="form-title">
-        <h1>Pesan Les</h1>
-        <p>Yuk jadwalkan les sekarang dan mulai belajar </p>
-        <form action="" className="form-section">
-            <div className="peserta-les">
-                <h3>Peserta les</h3>
-
-                <div className="radio-peserta">
-                    <input type="radio" name="peserta-les" value="individu" id="rd-individu"/> Individu
-                    <input type="radio" name="peserta-les" value="kelompok" id="rd-kelompok"/> Kelompok
-                </div>
-            </div>
-            <div className="materi">
-                <h3>Materi</h3>
-                <p>Jenjang pendidikan</p>
-
-                <div className="rd-pendidikan">
-                    <input type="radio" name="pendidikan" value="SD" id="rd-sd"/> SD
-                    <input type="radio" name="pendidikan" value="SMP" id="rd-smp"/> SMP
-                    <input type="radio" name="pendidikan" value="SMP" id="rd-sma"/> SMA
+    return (
+        <div className="form-order">
+            <h1>Pesan Les</h1>
+            <p>Yuk jadwalkan les sekarang dan mulai belajar </p>
+            <form action="" className="form-container">
+                <div className="pengajar-les">
+                    <img src="" alt="foto-pengajar" />
+                    <p>Albertus Handy Pramana Rendra</p>
+                    <p>Universitas Gadjah Mada | Ilmu Komputer</p>
+                    <p>Rp50.000 | 1 jam</p>
                 </div>
 
-                <p>Topik</p>
-                <select name="" id="select-topik">
-                    <option value="Matematika">Matematika</option>
-                    <option value="Fisika">Fisika</option>
-                    <option value="Kimia">Kimia</option>
-                    <option value="Biologi">Biologi</option>
-                    <option value="Ekonomi">Ekonomi</option>
-                    <option value="IPA">IPA</option>
-                    <option value="IPS">IPS</option>
-                </select>
+                <div className="materi">
+                    <h3 className="form-heading">Materi</h3>
+                    <p>Jenjang pendidikan</p>
 
-                <p>Detail Topik</p>
-                <textarea name="" id="detail-topik" cols="100" rows="5"
-                    placeholder="Jelaskan materi apa yang kamu ingin pelajari"></textarea>
-            </div>
-            <div className="waktu-les">
-                <h3>Waktu Les</h3>
+                    <div className="input-jenjang">
+                        <input type="radio" name="pendidikan" value="SD" id="rd-sd" />
+                        <label htmlFor="rd-sd">SD</label>
+                        <input type="radio" name="pendidikan" value="SMP" id="rd-smp" />
+                        <label htmlFor="rd-smp">SMP</label>
+                        <input type="radio" name="pendidikan" value="SMP" id="rd-sma" />
+                        <label htmlFor="rd-sma">SMA</label>
+                    </div>
 
-                <p>Tanggal Les</p>
-                <input type="date" id="date"/>
+                    <div className="input-topik">
+                        <p>Topik</p>
+                        <input type="text" placeholder="Matematika" />
+                    </div>
 
-                <p>Jam Mulai</p>
-                <input type="time" id="mulai"/>
+                    <div className="input-detail-topik">
+                        <p>Detail Topik</p>
+                        <textarea name="" id="detail-topik" cols="100" rows="5"
+                            placeholder="Jelaskan materi apa yang kamu ingin pelajari"></textarea>
+                    </div>
+                </div>
 
-                <p>Jam Selesai</p>
-                <input type="time" id="selesai"/>
+                <div className="waktu-les">
+                    <h3 className="form-heading">Waktu Les</h3>
 
-            </div>
-            <div className="lokasi-les">
-                <h3>Lokasi Les</h3>
+                    <div className="waktu-input">
+                        <div className="waktu-top">
+                            <p>Tanggal Les</p>
+                            <input type="date" id="date" />
+                        </div>
 
-                <input type="radio" name="lokasi" id="rd-on" value="online"/> Online
-                <input type="radio" name="lokasi" id="rd-off" value="offline"/> Offline
-            </div>
-            <div className="info">
-                <h3>Informasi Tambahan</h3>
-                <p>Catatan tambahan untuk pengajarmu</p>
-                <textarea name="" id="info-tambahan"></textarea>
-            </div>
-            <div className="button">
-                <button id="btn-batal">Batalkan Pesanan</button>
-                <button id="btn-pesan" type="submit">Pesan Sekarang</button>
-            </div>
-        </form>
-      </div>
-    </div>
-  );
+                        <div className="waktu-bottom">
+                            <div className="mulai-container">
+                                <p>Jam Mulai</p>
+                                <input type="time" id="mulai" />
+                            </div>
+
+                            <div className="durasi-container">
+                                <p>Durasi</p>
+                                <input type="number" placeholder="menit" className="durasi" />
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
+
+                <div className="lokasi-les">
+                    <h3 className="form-heading">Lokasi Les</h3>
+
+                    <div className="mode-pembelajaran">
+                        <input type="radio" name="lokasi" id="rd-on" value="online" />
+                        <label htmlFor="rd-on">Online</label>
+                        <input type="radio" name="lokasi" id="rd-off" value="offline" />
+                        <label htmlFor="rd-off">Offline</label>
+                    </div>
+
+                    <div className="detail-lokasi">
+                        <div className="alamat-lengkap">
+                            <p>Alamat</p>
+                            <textarea name="" id="" cols="30" rows="10" placeholder="Tuliskan alamatmu selengkap mungkin"></textarea>
+                        </div>
+                        <label htmlFor="maps">Lokasi Maps</label>
+                        <input type="text" name="maps" placeholder="Link google maps" />
+                    </div>
+                </div>
+
+                <div className="info-tambahan">
+                    <h3 className="form-heading">Informasi Tambahan</h3>
+                    <p>Catatan untuk pengajar</p>
+                    <textarea name="" id="info-tambahan" placeholder="Banyakin latihan soal ya"></textarea>
+                </div>
+                <div className="button">
+                    <Link to='/teacher' id="btn-batal">Batalkan Pesanan</Link>
+                    <button id="btn-pesan" type="submit">Pesan Sekarang</button>
+                </div>
+            </form>
+        </div>
+    );
 };
 
 export default OrderForm;
