7a8d512ed55938a5af6e5c162f866283cef675db
Fitron Ansori <fitronansori7@gmail.com>
2022-11-20 10:20:12 +0800

    update store

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -3,13 +3,13 @@ import thunk from "redux-thunk";
 import orderReducer from "../reducer/orderReducer";
 import userReducer from "../reducer/userReducer";
 
-import teacherReducer from "../reducer/teacherReducer"
+import teacherReducer from "../reducer/teacherReducer";
 
 const allReducer = combineReducers({
-    user: userReducer,
-    order: orderReducer,
-    teacher: teacherReducer,
-})
-const store = createStore(allReducer, applyMiddleware(thunk))
+  user: userReducer,
+  order: orderReducer,
+  teachers: teacherReducer,
+});
+const store = createStore(allReducer, applyMiddleware(thunk));
 
 export default store;
