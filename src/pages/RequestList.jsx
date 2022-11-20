aae7ade747b7d00a4627042454f2a4cba4fb8420
pramanarendra <albertushandypramana@gmail.com>
2022-11-20 15:45:09 +0700

    membuat fungsionalitas halaman list request

--- src/pages/RequestList.jsx
+++ src/pages/RequestList.jsx
@@ -3,9 +3,12 @@ import React, { useEffect, useState } from "react";
 // import DetailRequest from "../components/DetailRequest";
 import "../styles/RequestList.css";
 import "../styles/DetailRequest.css";
+import { useDispatch, useSelector } from "react-redux";
+import { getTeachers } from "../redux/action/teacherAction";
 
 function RequestList() {
   const [dataRequest, setDataRequest] = useState([]);
+  const [detail, setDetail] = useState(false)
 
   useEffect(() => {
     axios
@@ -19,68 +22,84 @@ function RequestList() {
       });
   }, []);
 
+  const { user } = useSelector((state) => state.user)
+
+  const dispatch = useDispatch();
+
+  const { teachers } = useSelector((state) => state.teachers);
+
+  const toggleDetail = () => {
+    setDetail(!detail)
+  }
+
+  useEffect(() => {
+    dispatch(getTeachers());
+  }, []);
+
+  let urutan = 0;
+
   return (
     <>
       <div className="order-container">
-        <h1 className="order-title">Daftar Pesanan</h1>
-        {dataRequest.map((item) => (
-          <div className="order-wrapper" key={item.id}>
-            <div className="order-top">
-              <div className="order-les">
-                <h3 className="order-les-title">Les {item.materi.topik}</h3>
-                <span className="order-les-date">
-                  dipesan tanggal {item.waktu.tanggal}
-                </span>
-              </div>
-              <div className="order-status">
-                <span>Terbayar</span>
-              </div>
-            </div>
-            <div className="order-bottom">
-              <div className="button-left">
-                <button className="btn btn-detail-pelamar">
-                  Lihat Daftar Pelamar
-                </button>
-              </div>
-              <div className="button-right">
-                <button className="btn-payment">Bayar Pesanan</button>
-              </div>
-            </div>
+        <h1 className="order-title">Daftar Sesi</h1>
+        {dataRequest.map((item, index) => {
+          if (item.userId == user.id) {
+            urutan++;
+            return (
+              <div className="pesanan" key={index}>
 
-            <div className="detail-container">
-              <div className="detail-item">
-                <div className="left-detail">
-                  <div className="first-item item">
-                    <h3>Peserta Les : </h3>
-                    <p> {item.peserta}</p>
-                  </div>
+                <p className="urutan-pesanan">Pesanan #{urutan}</p>
 
-                  <div className="second-item item">
-                    <h3>Detail Topik : </h3>
-                    <p>{item.materi.detail}</p>
-                  </div>
+                <div className="pesanan-waktu">
+                  <p className="pesanan-sub-judul">Waktu Sesi</p>
+                  <p className="tanggal">Tanggal: {item.tanggal}</p>
+                  <p className="mulai">Jam mulai: {item.jamMulai}</p>
+                  <p className="lama">Durasi sesi: {item.durasi}</p>
                 </div>
 
-                <div className="right-detail">
-                  <div className="third-item item">
-                    <h3>Waktu Les : </h3>
-                    <p> {item.waktu.tanggal}</p>
+                <p onClick={toggleDetail} className="toggle-detail">{detail ? <span>Tutup Detail</span> : <span>Lihat Detail</span>}</p>
 
-                    <h3>Jam : </h3>
-                    <p>
-                      {item.waktu.mulai} - {item.waktu.selesai}
-                    </p>
-                  </div>
+                {
+                  detail &&
 
-                  <div className="fourth-item item">
-                    <h3>Lokasi : </h3>
-                    <p>{item.lokasi}</p>
-                  </div>
-                </div>
+                  <>
+
+                    <div className="pesanan-materi">
+                      <p className="pesanan-sub-judul">Materi Sesi</p>
+                      <p className="jenjang-materi">Jenjang materi: {item.jenjangMateri}</p>
+                      <p className="topik-materi">Topik materi: {item.topik}</p>
+                      <p className="detail-materi">Detail materi: {item.detailTopik}</p>
+                      <p className="tambahan-materi">Tambahan: {item.tambahan}</p>
+                    </div>
+
+                    <div className="pesanan-lokasi">
+                      <p className="pesanan-sub-judul">Lokasi Sesi</p>
+                      <p className="mode-lokasi">Mode pembelajaran: {item.modeBelajar}</p>
+                      <p className="alamat-lokasi">alamat: {item.alamat}</p>
+                      <p className="maps-lokasi">maps: {item.maps}</p>
+                    </div>
+
+                    {teachers.map((teacher, index) => {
+                      if (teacher.id == item.teacherId)
+                        return (
+                          <div className="teacher-info" key={index}>
+                            <p>Pengajar</p>
+                            <img src={`https://drive.google.com/uc?export=view&id=${teacher.foto}`} alt="" className="info-foto" />
+                            <div className="info-text">
+                              <p className="nama">{teacher.nama}</p>
+                              <p className="edukasi">{teacher.edukasi[0].lokasi} | {teacher.edukasi[0].jurusan}</p>
+                            </div>
+                          </div>
+                        )
+                    })}
+                  </>
+                }
+
+
               </div>
-            </div>
-          </div>
-        ))}
+            )
+          }
+        })}
       </div>
     </>
   );
